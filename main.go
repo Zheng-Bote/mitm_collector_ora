6bc6fd9807044026e3d02bff34fe0a243c38f2c0
Zheng-Bote <zheng.bote@googlemail.com>
2026-09-20 10:05:13 +0200

    feat: Add flexible db_where_in support

--- main.go
+++ main.go
@@ -76,11 +76,12 @@ type SourceDBConfig struct {
 
 // CollectorArgs defines optional runtime arguments passed by the scheduler as JSON
 type CollectorArgs struct {
-	SourceName        string `json:"source_name"`
-	Table             string `json:"table"`
-	CursorColumn      string `json:"cursor_column"`
-	Topic             string `json:"topic"`
-	BusinessKeyColumn string `json:"business_key_column"`
+	SourceName        string              `json:"source_name"`
+	Table             string              `json:"table"`
+	CursorColumn      string              `json:"cursor_column"`
+	Topic             string              `json:"topic"`
+	BusinessKeyColumn string              `json:"business_key_column"`
+	DBWhereIn         map[string][]string `json:"db_where_in,omitempty"`
 }
 
 // StatusEvent is sent to the scheduler Unix socket
@@ -277,10 +278,14 @@ func main() {
 	cursorColumn := "" // No default, to allow tables without 'id'
 	topicName := "employee.data"
 	businessKeyCol := "id" // Default fallback
+	var dbWhereIn map[string][]string
 
 	if len(os.Args) >= 2 {
 		var colArgs CollectorArgs
 		if err := json.Unmarshal([]byte(os.Args[1]), &colArgs); err == nil {
+			if len(colArgs.DBWhereIn) > 0 {
+				dbWhereIn = colArgs.DBWhereIn
+			}
 			if colArgs.SourceName != "" {
 				targetCfg.SourceName = colArgs.SourceName
 			}
@@ -505,16 +510,43 @@ func main() {
 	// 13. Query Oracle table
 	var query string
 	var queryArgs []interface{}
-	if lastCursor != "" && cursorColumn != "" {
-		query = fmt.Sprintf("SELECT * FROM %s WHERE %s > :1 ORDER BY %s ASC",
-			tableName, cursorColumn, cursorColumn)
-		queryArgs = append(queryArgs, lastCursor)
-	} else if cursorColumn != "" {
-		query = fmt.Sprintf("SELECT * FROM %s ORDER BY %s ASC",
-			tableName, cursorColumn)
-	} else {
-		query = fmt.Sprintf("SELECT * FROM %s", tableName)
+	baseSQL := fmt.Sprintf("SELECT * FROM %s", tableName)
+	var conditions []string
+
+	if len(dbWhereIn) > 0 {
+		for col, vals := range dbWhereIn {
+			if !identifierRegex.MatchString(col) {
+				if ipc != nil {
+					ipc.SendEvent("failed", fmt.Sprintf("Invalid column name in db_where_in: %s", col), 0)
+				}
+				log.Fatalf("Invalid column name in db_where_in: %s", col)
+			}
+			if len(vals) == 0 {
+				continue
+			}
+			var placeholders []string
+			for _, val := range vals {
+				queryArgs = append(queryArgs, val)
+				placeholders = append(placeholders, fmt.Sprintf(":%d", len(queryArgs)))
+			}
+			conditions = append(conditions, fmt.Sprintf("%s IN (%s)", col, strings.Join(placeholders, ", ")))
+		}
 	}
+
+	if lastCursor != "" && cursorColumn != "" {
+		queryArgs = append(queryArgs, lastCursor)
+		conditions = append(conditions, fmt.Sprintf("%s > :%d", cursorColumn, len(queryArgs)))
+	}
+
+	if len(conditions) > 0 {
+		baseSQL += " WHERE " + strings.Join(conditions, " AND ")
+	}
+
+	if cursorColumn != "" {
+		baseSQL += fmt.Sprintf(" ORDER BY %s ASC", cursorColumn)
+	}
+
+	query = baseSQL
 
 	rows, err := oracleDB.Query(query, queryArgs...)
 	if err != nil {
