c76fdc1e5e5b2d04f5db750341402abc7bdd9975
Zheng-Bote <zheng.bote@googlemail.com>
2026-09-26 16:42:03 +0200

    chore(specdd): integrate specdd framework constraints, fix codebase alignment and implement zeroize/SPDX

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ MITM_VERSION=$(git describe --tags)
 
 CGO_ENABLED=0 go build -ldflags="-s -w -X main.version=${MITM_VERSION}" -o ./bin/mitm-collector-ora main.go
 
-cp bin/mitm-collector-ora ../../bin/bin/.
+cp bin/mitm-collector-ora ../../app/bin/.
